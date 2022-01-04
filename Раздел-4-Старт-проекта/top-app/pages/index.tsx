f0602814c3b80b7c4ac847b33b6c8cb2fa0011a6
Mikhail Golubtsov <mgolubcov@gmail.com>
2022-01-04 20:56:27 +0300

    44. Упражнение - Компонент p

--- Раздел-4-Старт-проекта/top-app/pages/index.tsx
+++ Раздел-4-Старт-проекта/top-app/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Htag } from "../components";
+import { Paragraph } from "../components";
 
 export default function Home(): JSX.Element {
   return (
@@ -8,6 +9,9 @@ export default function Home(): JSX.Element {
       <Button appearance='primary' className="testClass">Кнопка</Button>
       <Button appearance='primary' arrow='right'>Кнопка</Button>
       <Button appearance='ghost' arrow='right'>Кнопка</Button>
+      <Paragraph size="big">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Paragraph>
+      <Paragraph >Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Paragraph>
+      <Paragraph size="small">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Paragraph>
     </>
   );
 }
