ea10956e9cd8ebd2cf01bf6184618b55511be929
Mikhail Golubtsov <mgolubcov@gmail.com>
2022-01-05 18:33:06 +0300

    45. Компонент тэга

---
+++ Раздел-4-Старт-проекта/top-app/components/Tag/Tag.tsx
@@ -0,0 +1,22 @@
+import { TagProps } from "./Tag.props";
+import styles from './Tag.module.css';
+import cn from 'classnames';
+
+export const Tag = ({ size = 'small', color = 'ghost', href, className, children, ...props }: TagProps): JSX.Element => {
+  return (
+    <div
+      className={cn(styles.tag, className, {
+        [styles.small] : size === 'small',
+        [styles.medium] : size === 'medium',
+        [styles.ghost] : color === 'ghost',
+        [styles.red] : color === 'red',
+        [styles.grey] : color === 'grey',
+        [styles.green] : color === 'green',
+        [styles.primary] : color === 'primary',
+      })}
+      { ...props }
+    >
+      {href ? <a href={href}>{children}</a> : <>{children}</>}
+    </div>
+  );
+};
--- Раздел-4-Старт-проекта/top-app/pages/index.tsx
+++ Раздел-4-Старт-проекта/top-app/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Htag } from "../components";
+import { Button, Htag, Tag } from "../components";
 import { Paragraph } from "../components";
 
 export default function Home(): JSX.Element {
@@ -12,6 +12,12 @@ export default function Home(): JSX.Element {
       <Paragraph size="big">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Paragraph>
       <Paragraph >Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Paragraph>
       <Paragraph size="small">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Paragraph>
+      <Tag size="medium" > большой тег</Tag>
+      <Tag href="#test!"> ссылка</Tag>
+      <Tag color="red"> тест</Tag>
+      <Tag color="grey"> тест</Tag>
+      <Tag color="green"> тест</Tag>
+      <Tag color="primary"> тест</Tag>
     </>
   );
 }
