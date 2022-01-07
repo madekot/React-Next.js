46baed9e97a51d01f0055b361556382ce4074eca
Mikhail Golubtsov <mgolubcov@gmail.com>
2022-01-07 16:59:07 +0300

    63. Упражнение - Вёрстка footer

--- Раздел-4-Старт-проекта/top-app/layout/Layout.tsx
+++ Раздел-4-Старт-проекта/top-app/layout/Layout.tsx
@@ -1,6 +1,5 @@
 import { LayoutProps } from './Layout.props';
 import styles from './Layout.module.css';
-import cn from 'classnames';
 import { Header } from './Header/Header';
 import { Sidebar } from './Sidebar/Sidebar';
 import { Footer } from './Footer/Footer';
@@ -8,16 +7,14 @@ import { FunctionComponent } from 'react';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>
-          {children}
-        </div>
+    <div className={styles.wrapper}>
+      <Header className={styles.header}/>
+      <Sidebar className={styles.sidebar}/>
+      <div className={styles.body}>
+        {children}
       </div>
-      <Footer />
-    </>
+      <Footer className={styles.footer}/>
+    </div>
   );
 };
 
