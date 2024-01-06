a201f75ce04ab6938d285cf04cd7b5381b119382
Mikhail Golubtsov <mgolubcov@gmail.com>
2024-01-06 17:38:17 +0300

    feat: Добавляет функциональности в ReviewForm
    ReviewForm отображает сообщение об ошибках на валидированных полях

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -7,7 +7,7 @@ import { useForm, Controller } from "react-hook-form";
 import { IReviewForm } from "@/components/ReviewForm/ReviewForm.interface";
 
 export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps): JSX.Element => {
-    const { register, control, handleSubmit } = useForm<IReviewForm>();
+    const { register, control, handleSubmit, formState: { errors } } = useForm<IReviewForm>();
 
     const onSubmit = (data: IReviewForm) => {
         console.log(data);
@@ -18,20 +18,41 @@ export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps):
             <div className={cn(styles.reviewForm, className)}
                  {...props}
             >
-                <Input {...register('name')} placeholder='Имя' />
-                <Input {...register('title')} placeholder='Заголовок отзыва' className={styles.title} />
+                <Input
+                    {...register('name', { required: { value: true, message: 'Заполните имя' } })}
+                    placeholder='Имя'
+                    error={errors.name}
+                />
+                <Input
+                    {...register('title', { required: { value: true, message: 'Заполните заголовок' } })}
+                    placeholder='Заголовок отзыва'
+                    className={styles.title}
+                    error={errors.title}
+                />
                 <div className={styles.rating}>
                     <span>Оценка:</span>
                     <Controller
                         control={control}
                         name='rating'
+                        rules={{ required: { value: true, message: 'Забыли оценку' } }}
                         render={({ field }) => (
-                            <Rate isEditable rating={field.value} ref={field.ref} handleRatingChange={field.onChange} />
+                            <Rate
+                                isEditable
+                                rating={field.value}
+                                ref={field.ref}
+                                handleRatingChange={field.onChange}
+                                error={errors.rating}
+                            />
                         )}
                     />
 
                 </div>
-                <Textarea {...register('description')} placeholder='Текст отзыва' className={styles.description} />
+                <Textarea
+                    {...register('description', { required: { value: true, message: 'Заполните описание' } })}
+                    placeholder='Текст отзыва'
+                    className={styles.description}
+                    error={errors.description}
+                />
                 <div className={styles.submit}>
                     <Button variant="primary">Отправить</Button>
                     <span className={styles.info}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
