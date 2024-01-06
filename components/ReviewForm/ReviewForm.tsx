dc810d25e30a2f1ab23cc505aa477ce12059eaec
Mikhail Golubtsov <mgolubcov@gmail.com>
2024-01-06 18:35:59 +0300

    feat: Добавляет функциональности в ReviewForm
    - Добавлена возможность отправки отзыва на сейрвер
    - добавлен UI успешного и провального отправления отзыва

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -4,13 +4,35 @@ import CloseIcon from './close.svg';
 import cn from 'classnames';
 import { Button, Input, Rate, Textarea } from '@/components';
 import { useForm, Controller } from "react-hook-form";
-import { IReviewForm } from "@/components/ReviewForm/ReviewForm.interface";
+import { IReviewForm, IReviewSentResponse } from "./ReviewForm.interface";
+import { useState } from "react";
+import axios from "axios";
+import { API } from "@/helpers";
 
 export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps): JSX.Element => {
-    const { register, control, handleSubmit, formState: { errors } } = useForm<IReviewForm>();
+    const {
+        register,
+        control,
+        handleSubmit,
+        formState: { errors },
+        reset
+    } = useForm<IReviewForm>();
 
-    const onSubmit = (data: IReviewForm) => {
-        console.log(data);
+    const [isSuccess, setIsSuccess] = useState<boolean>(false);
+    const [error, setError] = useState<string>();
+
+    const onSubmit = async (formData: IReviewForm) => {
+        try {
+            const { data } = await axios.post<IReviewSentResponse>(API.review.createDemo, { ...formData, productId });
+            if (data.message) {
+                setIsSuccess(true);
+                reset();
+            } else {
+                setError('Что-то пошло не так');
+            }
+        } catch (e) {
+            e instanceof Error && setError(e.message);
+        }
     };
 
     return (
@@ -58,13 +80,15 @@ export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps):
                     <span className={styles.info}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
                 </div>
             </div>
-            <div className={styles.success}>
+            {isSuccess && <div className={cn(styles.success, styles.panel)}>
                 <div className={styles.successTitle}>Ваш отзыв отправлен</div>
-                <div>
-                    Спасибо, ваш отзыв будет опубликован после проверки.
-                </div>
-                <CloseIcon className={styles.close} />
-            </div>
+                <div> Спасибо, ваш отзыв будет опубликован после проверки. </div>
+                <CloseIcon className={styles.close} onClick={() => setIsSuccess(false)} />
+            </div>}
+            {error && <div className={cn(styles.error, styles.panel)}>
+              Что-то пошло не так, попробуйте обновить страницу
+              <CloseIcon className={styles.close} onClick={() => setError(undefined)} />
+            </div>}
         </form>
     );
 };
